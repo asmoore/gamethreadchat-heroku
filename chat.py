9e9de7aa750321edb514ba959d671a3e7a39cfef
Andrew Moore <asmoor@gmail.com>
2015-02-25 19:27:37 -0500

    test comment

--- chat.py
+++ chat.py
@@ -10,7 +10,15 @@ def echo_socket(ws):
 
     while True:
         #message = ws.receive()
-        ws.send("message")
+        comment_dict = {"author": "catmoon", 
+                        "body": "test", 
+                        "author_flair_css_class": "Heat1", 
+                        "comment_id": "qwresdf", 
+                        "score": "sdfsafd",
+                        "created_utc": "123123123", 
+                        "emitted": "true"}
+        message = json.dumps({'message': comment_dict,'category':'comment', 'thread': 'asda'})
+        ws.send(message)
         time.sleep(5)
 
 @app.route('/chat')
